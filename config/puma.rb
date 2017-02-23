83f3a9d0bf3f15971abcd4a72b2fcf8d53a2ff64
jb-lyp <17717045404@163.com>
2017-02-23 13:53:05 +0800

    edit puma config

--- config/puma.rb
+++ config/puma.rb
@@ -4,28 +4,28 @@
 
 #rails的运行环境
 #if Rails.env == 'production'
-# environment 'production'
-# threads 2, 64
-# workers 4
+environment 'production'
+threads 2, 64
+workers 4
 
 #项目名
-# app_name = "my_blog"
+app_name = "my_blog"
 #项目路径
-# application_path = "/var/lyp-rails-app/#{app_name}"
+application_path = "/var/lyp-rails-app/#{app_name}"
 #这里一定要配置为项目路径下地current
-# directory "#{application_path}/current"
+directory "#{application_path}/current"
 
 #下面都是 puma的配置项
-# pidfile "#{application_path}/shared/tmp/pids/puma.pid"
-# state_path "#{application_path}/shared/tmp/sockets/puma.state"
-# stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
-# bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
-# activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
+pidfile "#{application_path}/shared/tmp/pids/puma.pid"
+state_path "#{application_path}/shared/tmp/sockets/puma.state"
+stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
+bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
+activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
 
 #后台运行
-# daemonize true
-# on_restart do
-#   puts 'On restart...'
-# end
-# preload_app!
+daemonize true
+on_restart do
+  puts 'On restart...'
+end
+preload_app!
 
